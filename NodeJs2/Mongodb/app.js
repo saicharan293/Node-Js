b9a1fc18c5f90d20894a29f745736c0939cbd039
saicharan293 <2493saicharan@gmail.com>
2024-05-03 18:07:49 +0530

    model_creation_mongodb_CRUD

---
+++ NodeJs2/Mongodb/app.js
@@ -0,0 +1,50 @@
+const express=require('express');
+const app=express();
+const userModel= require('./usermodel');
+const usermodel = require('./usermodel');
+
+app.get('/',(req,res)=>{
+    res.send("Heyyy ")
+})
+
+//create
+app.get('/create', async(req,res)=>{
+    let createduser= await userModel.create({
+        name:'sai charan',
+        email:'charan@gmail.com',
+        username:'charan'
+    })
+    // console.log('create route hey')
+    res.send(createduser)
+})
+
+//update
+app.get('/update', async(req,res)=>{
+    let updatedUser=await userModel.findOneAndUpdate({username:'sai'},{name:'sai charan amudala'},{new : true})
+    // console.log('create route hey')
+    res.send(updatedUser)
+})
+
+//read-all
+app.get('/read',async(req,res)=>{
+    let allusers=await userModel.find()
+    res.send(allusers)
+})
+
+//read
+app.get('/read/name',async(req,res)=>{
+    let allusers=await userModel.findOne({username:'charan'})
+    res.send(allusers)
+})
+
+//delete
+app.get('/delete',async(req,res)=>{
+    let deletedUser=await userModel.findOneAndDelete({username:'charan'})
+    res.send(deletedUser)
+})
+
+
+//server
+app.listen(3000,()=>{
+    console.log('server started')
+})
