978a1e9d6874d4b6fe0f1dc92a94897148ac56da
saicharan293 <2493saicharan@gmail.com>
2024-05-05 22:35:13 +0530

    update_CRUD_complete

--- NodeJs2/Mongodb2/app2.js
+++ NodeJs2/Mongodb2/app2.js
@@ -32,16 +32,34 @@ app.post("/create", async (req, res) => {
 });
 
 //? read users
-app.get('/read',async (req,res)=>{
-  let users=await userModel.find();
-  // res.render("read",{users})
-})
+app.get("/read", async (req, res) => {
+  let users = await userModel.find();
+  res.render("read", { users });
+});
+app.get("/edit/:userid", async (req, res) => {
+  let user = await userModel.findOne({ _id: req.params.userid });
+  console.log("edit selected")
+  res.render("edit", { user });
+});
+app.post("/update/:id", async (req, res) => {
+  console.log('update started',req.body);
+  let { name, image, email } = req.body;
+  let users = await userModel.findOneAndUpdate(
+    { _id: req.params.id },
+    { name: name,
+      email: email,
+      image: image, },
+    { new: true }
+  );
+  console.log('update complete',users)
+  res.redirect("/read");
+});
 
 //? delete user by id
-app.get('/delete/:id',async(req,res)=>{
-  let users=await userModel.findOneAndDelete({_id:req.params.id});
-  res.redirect('/read')
-})
+app.get("/delete/:id", async (req, res) => {
+  let users = await userModel.findOneAndDelete({ _id: req.params.id });
+  res.redirect("/read");
+});
 
 app.listen(3000, () => {
   console.log("server started for app2");
