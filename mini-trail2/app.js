d56591c68492b0f186f0b73a603efc5c277182fa
saicharan293 <2493saicharan@gmail.com>
2024-05-21 10:57:35 +0530

    Like and Unlike added to the profile page

--- mini-trail2/app.js
+++ mini-trail2/app.js
@@ -25,6 +25,18 @@ app.get('/profile',isLoggedIn, async(req,res)=>{
     let user=await userModel.findOne({email:req.user.email}).populate('posts')
     res.render("profile",{user})
 })
+app.get('/like/:id',isLoggedIn, async(req,res)=>{
+    let post=await postModel.findOne({_id:req.params.id}).populate('user')
+    if(post.likes.indexOf(req.user.userid)===-1){
+        post.likes.push(req.user.userid)
+    }else{
+        post.likes.splice(post.likes.indexOf(req.user.userid),1)
+    }
+    // post.likes.push(req.user.userid)
+    await post.save();
+    res.redirect("/profile")
+})
+
 app.post('/post',isLoggedIn, async(req,res)=>{
     let user=await userModel.findOne({email:req.user.email})
     let {content}=req.body;
@@ -36,6 +48,7 @@ app.post('/post',isLoggedIn, async(req,res)=>{
     await user.save();
     res.redirect('/profile')
 })
+
 app.post('/register',async(req,res)=>{
     let{email,age,password,name,username}=req.body;
     let user=await userModel.findOne({email})
