27457f301b1afffb0eaad4a8b6679d0b8b18f608
saicharan293 <2493saicharan@gmail.com>
2024-06-10 20:33:33 +0530

    scatch 2 file started along with config file

---
+++ scatch2/app.js
@@ -0,0 +1,21 @@
+const express=require('express');
+const app=express();
+
+const cookieParser=require('cookie-parser');
+const path=require('path');
+
+const db=require('./config/mongooseConnection')
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname,'public')))
+
+app.set('view engine','ejs')
+app.get('/',(req,res)=>{
+    res.send('hey')
+})
+
+app.listen(3000,()=>{
+    // console.log("scatch 2 shuru hua abhi")
+})
---
+++ scatch2/config/mongooseConnection.js
@@ -0,0 +1,9 @@
+const mongoose=require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1:27017/2scatch2').then(function(){
+    console.log("connected at mongoose Connection")
+}).catch(function(err){
+    console.log('error',err);
+})
+
+module.exports=mongoose.connection;
