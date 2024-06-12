6af95369613a5caf802f9bf3043e466b371a5eaa
saicharan293 <2493saicharan@gmail.com>
2024-06-12 18:35:44 +0530

    login page is added

--- Express/newApp/routes/index.js
+++ Express/newApp/routes/index.js
@@ -10,6 +10,9 @@ passport.authenticate(new localStrategy(userModel.authenticate()));
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/profile',isLoggedIn, function(req, res, next) {
+  res.send('profile');
+});
 
 router.post('/register' ,(req,res)=>{
   let{username,email,fullname}=req.body
@@ -30,7 +33,7 @@ router.post('/login' ,passport.authenticate('local',{
   }),(req,res)=>{
 })
 
-route.get('/logout',function(req,res){
+router.get('/logout',function(req,res){
   req.logout(function(err){
     if(err){
       return next(err);
@@ -38,5 +41,11 @@ route.get('/logout',function(req,res){
     res.redirect('/');
   });
 })
+
+function isLoggedIn(req,res,next){
+  if(req.isAuthenticated()) return next()
+  
+  res.redirect('/')
+}
 
 module.exports = router;
