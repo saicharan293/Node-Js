f5d7851c3810629fc036234caff7a4f744995f73
saicharan293 <2493saicharan@gmail.com>
2024-06-22 17:11:54 +0530

    create,search one user, all users, delete user is done

--- backend_1/express_gen_coding/routes/index.js
+++ backend_1/express_gen_coding/routes/index.js
@@ -1,9 +1,40 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const userModel = require("./users");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index',{title:'express'});
+router.get("/", function (req, res, next) {
+  res.render("index");
 });
+
+//create user
+router.get("/create", async function (req, res, next) {
+  let user = await userModel.create({
+    username: "abc",
+    name: "abc",
+    age: 23,
+  });
+  res.send(user);
+});
+
+
+// all users
+router.get("/all",async function (req, res, next) {
+  let allusers=await userModel.find();
+  res.send(allusers);
+});
+
+//search for specific user
+router.get("/user",async function (req, res, next) {
+  let allusers=await userModel.findOne({username:'abc'});
+  res.send(allusers);
+});
+
+//delete specific user
+router.get("/delete",async function (req, res, next) {
+  let deleteduser=await userModel.findOneAndDelete({username:'abc'});
+  res.send(deleteduser);
+});
+
 
 module.exports = router;
--- backend_1/express_gen_coding/routes/users.js
+++ backend_1/express_gen_coding/routes/users.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
+const mongoose=require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/backend_1')
+const userSchema=mongoose.Schema({
+  username:String,
+  name:String,
+  age: Number
+})
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
-module.exports = router;
+module.exports=mongoose.model('user',userSchema)
