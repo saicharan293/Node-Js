279954a2422845a6837cfe2659b77d82e2c03dde
saicharan293 <2493saicharan@gmail.com>
2024-06-25 00:49:18 +0530

    passport set up is completd

--- nayaapp/routes/users.js
+++ nayaapp/routes/users.js
@@ -1,4 +1,7 @@
 const mongoose = require("mongoose");
+
+const plm=require('passport-local-mongoose');
+
 mongoose.connect("mongodb://127.0.0.1:27017/nayaapp");
 
 const userSchema = mongoose.Schema(
@@ -33,5 +36,6 @@ const userSchema = mongoose.Schema(
     timestamps: true,
   }
 );
+userSchema.plugin(plm);
 
 module.exports = mongoose.model("User", userSchema);
