0805e52a083290f2c90a57c6c2472092046cf35e
saicharan293 <2493saicharan@gmail.com>
2024-06-26 21:54:37 +0530

    Login page and login css is created

--- nayaapp/routes/index.js
+++ nayaapp/routes/index.js
@@ -4,11 +4,14 @@ const userModel = require("./users");
 const postModel = require("./post");
 const passport = require("passport");
 const localStrategy = require("passport-local");
-passport.authenticate(new localStrategy(userModel.authenticate()));
+passport.use(new localStrategy(userModel.authenticate()));
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
+});
+router.get("/login", function (req, res, next) {
+  res.render("login");
 });
 
 //profile route
--- nayaapp/routes/users.js
+++ nayaapp/routes/users.js
@@ -13,7 +13,6 @@ const userSchema = mongoose.Schema(
     },
     password: {
       type: String,
-      required: true,
     },
     posts: [{
       type:mongoose.Schema.Types.ObjectId,
