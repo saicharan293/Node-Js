389429c1d24755451b532c31539bbc5790e90172
saicharan293 <2493saicharan@gmail.com>
2024-07-09 23:12:16 +0530

    login page and related routes are created

--- pin/routes/index.js
+++ pin/routes/index.js
@@ -1,11 +1,16 @@
 var express = require("express");
 var router = express.Router();
+const userModel=require('./users')
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index");
 });
 router.get("/register", function (req, res, next) {
+  res.render("register");
+});
+router.post("/register", function (req, res, next) {
+  const data=new userModel
   res.render("register");
 });
 
