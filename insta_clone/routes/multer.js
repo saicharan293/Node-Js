cae289d0d83227640c73085f0a340a8d4d6a7a01
saicharan293 <2493saicharan@gmail.com>
2024-07-14 19:46:10 +0530

    feed page is displayed, stories are still need to work on

--- insta_clone/routes/multer.js
+++ insta_clone/routes/multer.js
@@ -1,6 +1,6 @@
 const multer = require("multer");
 const path = require("path");
-const {v4:uuidv4} = require('uuid');
+const { v4: uuidv4 } = require("uuid");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -8,8 +8,8 @@ const storage = multer.diskStorage({
   },
   filename: function (req, file, cb) {
     const unique = uuidv4();
-    const pathname=  path.extname(file.originalname);
-    cb(null, unique+pathname);
+    const pathname = path.extname(file.originalname);
+    cb(null, unique + pathname);
   },
 });
 
--- insta_clone/routes/users.js
+++ insta_clone/routes/users.js
@@ -1,48 +1,50 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const plm = require("passport-local-mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/instainsta");
+mongoose.connect("mongodb://127.0.0.1:27017/insta");
 
 const userSchema = mongoose.Schema({
   username: String,
-  name: String,
+  fullname: String,
   email: String,
   password: String,
-  picture: {
+  profileImage: {
     type: String,
-    default: "def.png"
+    default: "def.png",
   },
   contact: String,
   bio: String,
   stories: [
     {
       type: mongoose.Schema.Types.ObjectId,
-      ref: "story" 
-    }
+      ref: "story",
+    },
   ],
   saved: [
     {
       type: mongoose.Schema.Types.ObjectId,
-      ref: "post" 
-    }
+      ref: "post",
+    },
   ],
-  posts: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "post" 
-  }],
+  posts: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "post",
+    },
+  ],
   followers: [
     {
       type: mongoose.Schema.Types.ObjectId,
-      ref: "user" 
-    } 
+      ref: "user",
+    },
   ],
   following: [
     {
       type: mongoose.Schema.Types.ObjectId,
-      ref: "user" 
-    }
-  ]
-})
+      ref: "user",
+    },
+  ],
+});
 
 userSchema.plugin(plm);
 
