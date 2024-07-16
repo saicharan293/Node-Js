02d44cb50b20e2c9cfde7828851042432b623fcd
saicharan293 <2493saicharan@gmail.com>
2024-07-16 20:06:58 +0530

    login,register,logout routes are added along with register page

--- pin2/routes/index.js
+++ pin2/routes/index.js
@@ -1,9 +1,43 @@
 var express = require('express');
 var router = express.Router();
+const userModel=require('./users');
+const passport = require('passport');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
 });
+
+router.get('/register', function(req, res, next) {
+  res.render('register');
+});
+
+router.post('/register', function(req, res, next) {
+  const user= new userModel({
+    username:req.body.username,
+    email:req.body.email,
+    contact:req.body.contact
+  })
+  userModel.register(user,req.body.password)
+  .then(function(){
+    passport.authenticate('local')(req,res,function(){
+      res.redirect('/profile');
+    })
+  })
+});
+
+router.post('/profile',passport.authenticate('local',{
+  failureRedirect:'/',
+  successRedirect:'/profile'
+}), function(req, res, next) {
+});
+
+router.get('/logout',function(req,res,next){
+  req.logout(function(err) {
+    if (err) { return next(err); }
+    res.redirect('/');
+  });
+})
+
 
 module.exports = router;
--- pin2/routes/users.js
+++ pin2/routes/users.js
@@ -1,9 +1,16 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
-module.exports = router;
+const mongoose=require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/pin2')
+const plm=require('passport-local-mongoose');
+const userSchema=mongoose.Schema({
+  username:String,
+  fullname:String,
+  password:String,
+  profileImage:String,
+  contact:Number,
+  board:{
+    type:Array,
+    default:[]
+  },
+})
+userSchema.plugin(plm);
+module.exports=mongoose.model('user',userSchema)
