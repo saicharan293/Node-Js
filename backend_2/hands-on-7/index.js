05dddff20ec5501569c8f2327382ecdb996b46dd
saicharan293 <2493saicharan@gmail.com>
2024-07-28 02:04:56 +0530

    create route is created and file is set to be saved with capitalzed nomenclature of file

--- backend_2/hands-on-7/index.js
+++ backend_2/hands-on-7/index.js
@@ -1,17 +1,29 @@
-const express=require('express');
-const app=express();
-const path=require('path');
+const express = require("express");
+const app = express();
+const path = require("path");
+const fs = require("fs");
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-app.use(express.static(path.join(__dirname,'public')))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.set('view engine','ejs')
+app.set("view engine", "ejs");
 
-app.get('/',(req,res)=>{
-    res.render('index')
-})
+app.get("/", (req, res) => {
+  fs.readdir(`./files`, function (err, files) {
+    res.render("index", { files });
+  });
+});
 
-app.listen(3000,()=>{
-    console.log("server shuru karo")
-})
+//title variable is converted with capitalized sentence
+app.post("/create", (req, res) => {
+  console.log(req.body);
+  fs.writeFile(`./files/${req.body.title.split(" ").map((word) => word.charAt(0).toUpperCase() + word.slice(1)).join("")}.txt`,req.body.details,function(err){
+    res.redirect('/')
+      }
+  ); 
+});
+
+app.listen(3000, () => {
+  console.log("server shuru karo");
+});
