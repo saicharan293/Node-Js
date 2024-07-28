baebdab7f21f52b8dead1d9e8e1410673595269b
saicharan293 <2493saicharan@gmail.com>
2024-07-28 14:22:15 +0530

    edit form is completed

--- backend_2/hands-on-7/index.js
+++ backend_2/hands-on-7/index.js
@@ -21,6 +21,17 @@ app.get("/file/:filename", (req, res) => {
   })
 });
 
+app.get("/edit/:filename", (req, res) => {
+  res.render('edit',{filename:req.params.filename});
+});
+
+app.post("/edit", (req, res) => {
+  console.log(req.body)
+  fs.rename(`./files/${req.body.previous}`,`./files/${req.body.new}`,function(err){
+    res.redirect('/');
+  })
+});
+
 //title variable is converted with capitalized sentence
 app.post("/create", (req, res) => {
   console.log(req.body);
