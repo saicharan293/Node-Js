a2e1ec6357065a129b8c69169daae6f3b2a10179
saicharan293 <2493saicharan@gmail.com>
2024-07-28 23:04:17 +0530

    read ejs page in progress

--- backend_2/CRUDwithEjs-12/index.js
+++ backend_2/CRUDwithEjs-12/index.js
@@ -2,13 +2,20 @@ const express=require('express');
 const app=express();
 const path=require('path');
 
+//practice purpose, not yet used
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname,'public')));
 
+app.set('view engine','ejs');
 
 app.get('/',(req,res)=>{
-    res.send("CRUD with ejs chalu kar")
+    res.render("index")
+})
+
+//read 
+app.get('/read',(req,res)=>{
+    res.render("read")
 })
 
 app.listen(3000,()=>{
