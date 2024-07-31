c323faeee5ff7fa55c75b0e11d22e4032fde79f4
saicharan293 <2493saicharan@gmail.com>
2024-07-31 23:05:56 +0530

    mini project started and basic boiler plate completed

---
+++ backend_2/MiniPrj-1-17/index.js
@@ -0,0 +1,16 @@
+const express=require('express');
+const app=express();
+const path=require('path');
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
+
+app.set('view engine','ejs');
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname,'public')))
+
+app.get('/',(req,res)=>{
+    res.send("hello mini")
+ });
+
+app.listen(3000,()=>console.log("server shuru hui"));
