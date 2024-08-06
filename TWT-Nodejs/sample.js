28803e9f3097fe50ee314c2108af55a4f7d56541
saicharan293 <2493saicharan@gmail.com>
2024-08-06 20:04:10 +0530

    node js telugu web tech started

---
+++ TWT-Nodejs/sample.js
@@ -0,0 +1,21 @@
+//import os module
+const os=require('os');
+const path=require('path');
+
+//gives os type in the pc
+console.log(os.type())
+
+//gives version of pc
+console.log(os.version())
+
+//gives free memory present in os
+console.log(os.freemem())
+
+//gives cpu status
+// console.log(os.cpus())
+
+//current directory
+console.log(__dirname)
+
+//current file
+console.log(__filename)
