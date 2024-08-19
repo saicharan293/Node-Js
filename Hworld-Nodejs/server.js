d183140380330695215a20fefb55563656f31533
saicharan293 <2493saicharan@gmail.com>
2024-08-19 23:21:54 +0530

    explanation about methods

--- Hworld-Nodejs/server.js
+++ Hworld-Nodejs/server.js
@@ -1,7 +1,12 @@
 const jsonString='{"name":"John","age":30,"city":"New York"}';
+
+// JSON.parse() is used to convert a (JSON-formatted) string into a JavaScript object.
+
 const jsonObject=JSON.parse(jsonString);
 console.log('json object name is ',jsonObject.name);
 
+
+// JSON.stringify() is used to convert a JavaScript object into a (JSON-formatted) string.
 const objectToConvert={name:"alice",age:24};
 const jsonStringified=JSON.stringify(objectToConvert);
 console.log(jsonStringified)
