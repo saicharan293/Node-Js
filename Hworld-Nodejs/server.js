656506a5459054096603b75b081005fda01e66eb
saicharan293 <2493saicharan@gmail.com>
2024-08-22 00:14:11 +0530

    studied post and get api methods using postman

--- Hworld-Nodejs/server.js
+++ Hworld-Nodejs/server.js
@@ -9,5 +9,28 @@ app.get('/idli', function (req, res) {
   res.send('I would serve Idli ')
 })
 
-app.listen(3000)
+app.get('/ravvaidli',(req,res)=>{
+  var customised_Khana={
+    name:'ravva idli',
+    size:'10 cm diameter',
+    sambar_chahiye:true,
+    chutney_chahiye:true
+  }
+  res.send(customised_Khana)
+})
+
+app.post('/person',(req,res)=>{
+  var person={
+    name:"bot",
+    age:25,
+    salary:25000,
+    role: 'kuch bhi',
+    address:'destination'
+  }
+  res.send(person)
+})
+
+app.listen(3000,()=>{
+  console.log('server shuru')
+})
 
