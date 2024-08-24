4df2feb4172e2cb99a628d6ee4040a4c6ae41bd0
saicharan293 <2493saicharan@gmail.com>
2024-08-24 23:56:39 +0530

    added mongodb to node js

---
+++ Hworld-Nodejs/db.js
@@ -0,0 +1,21 @@
+const mongoose=require('mongoose');
+
+
+const mongoUrl='mongodb://127.0.0.1/hworld-hotels'
+mongoose.connect(mongoUrl,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+})
+
+const db=mongoose.connection;
+
+//event listener
+db.on('connected',()=>{
+    console.log('Mongo db server connected')
+});
+
+db.on('disconnected',()=>{
+    console.log('Mongo db server disconnected');
+});
+
+module.exports=db
--- Hworld-Nodejs/server.js
+++ Hworld-Nodejs/server.js
@@ -1,33 +1,10 @@
 const express = require('express')
 const app = express()
 
+const db=require('./db');
+
 app.get('/', function (req, res) {
   res.send('Heyy, what do you like to eat? ')
-})
-
-app.get('/idli', function (req, res) {
-  res.send('I would serve Idli ')
-})
-
-app.get('/ravvaidli',(req,res)=>{
-  var customised_Khana={
-    name:'ravva idli',
-    size:'10 cm diameter',
-    sambar_chahiye:true,
-    chutney_chahiye:true
-  }
-  res.send(customised_Khana)
-})
-
-app.post('/person',(req,res)=>{
-  var person={
-    name:"bot",
-    age:25,
-    salary:25000,
-    role: 'kuch bhi',
-    address:'destination'
-  }
-  res.send(person)
 })
 
 app.listen(3000,()=>{
