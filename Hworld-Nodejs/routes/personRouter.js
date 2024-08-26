3322d9450c7b0902ac38b8095f4577937cbe867c
saicharan293 <2493saicharan@gmail.com>
2024-08-27 00:39:21 +0530

    update and delete operations are done

--- Hworld-Nodejs/routes/personRouter.js
+++ Hworld-Nodejs/routes/personRouter.js
@@ -49,4 +49,40 @@ router.get('/:worktype',async (req,res)=>{
     }
 })
 
+//update person
+router.put('/:id',async (req,res)=>{
+  try{
+    const personId=req.params.id;
+    const updatePerson=req.body;
+    const response=await personModel.findByIdAndUpdate(personId,updatePerson,{
+      new:true, //return updated document
+      runValidators:true,// run mongoose validation
+    })
+    if(!response){
+      return res.status(404).json({error:'Person not found'})
+    }
+    console.log('data updated');
+    res.status(200).json(response);
+  }catch(err){
+    console.log(err);
+    res.status(500).json({err:'Internal server error'})
+  }
+})
+
+//delete person
+router.delete('/:id',async (req,res)=>{
+  try{
+    const personId=req.params.id;
+    const response=await personModel.findByIdAndDelete(personId);
+    if(!response){
+      return res.status(404).json({err:'Person not found'});
+    } 
+    console.log('data deleted');
+    res.status(200).json({message:'Person deleted successfully'});
+  }catch(err){
+    console.log(err);
+    res.status(500).json({err:'Internal Server error'})
+  }
+})
+
 module.exports=router;
