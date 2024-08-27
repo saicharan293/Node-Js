229a043c85503b2c5d984584d52ecd45984760a9
saicharan293 <2493saicharan@gmail.com>
2024-08-27 23:17:45 +0530

    connected to online mongodb database

--- Hworld-Nodejs/db.js
+++ Hworld-Nodejs/db.js
@@ -1,21 +1,23 @@
-const mongoose=require('mongoose');
+const mongoose = require("mongoose");
 
+// const mongoUrl='mongodb://127.0.0.1/hworld-hotels'
+//web hosting for mongo
+const mongoUrl =
+  "mongodb+srv://saicharan:shivaay123@mymongo.uv55x.mongodb.net/";
+mongoose.connect(mongoUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-const mongoUrl='mongodb://127.0.0.1/hworld-hotels'
-mongoose.connect(mongoUrl,{
-    useNewUrlParser:true,
-    useUnifiedTopology:true
-})
-
-const db=mongoose.connection;
+const db = mongoose.connection;
 
 //event listener
-db.on('connected',()=>{
-    console.log('Mongo db server connected')
+db.on("connected", () => {
+  console.log("Mongo db server connected");
 });
 
-db.on('disconnected',()=>{
-    console.log('Mongo db server disconnected');
+db.on("disconnected", () => {
+  console.log("Mongo db server disconnected");
 });
 
-module.exports=db
+module.exports = db;
