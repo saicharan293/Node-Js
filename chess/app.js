d31db8c2df8292be56e8597e92f32b3007080681
saicharan293 <2493saicharan@gmail.com>
2024-09-01 23:03:47 +0530

    Created upto chessboard view

--- chess/app.js
+++ chess/app.js
@@ -24,8 +24,43 @@ app.get('/',(req,res)=>{
 
 io.on('connection',function(uniqueSocket){
     console.log('connected');
+    if(!players.white){
+        players.white=uniqueSocket.id;
+        uniqueSocket.emit('playerRole','w');
+    }
+    else if(!players.black){
+        players.black=uniqueSocket.id;
+        uniqueSocket.emit('playerRole','b')
+    }else{
+        uniqueSocket.emit('spectatorRole');
+    }
     uniqueSocket.on('disconnect',function(){
-        console.log('disconnected');
+        if(uniqueSocket.id==players.white){
+            delete players.white;
+        }
+        else if(uniqueSocket.id==players.black){
+            delete players.black;
+        }
+    });
+
+    uniqueSocket.on('move',(move)=>{
+        try {
+            if(chess.turn()=='w' && uniqueSocket.id!==players.white) return;
+            if(chess.turn()=='b' && uniqueSocket.id!==players.black) return;
+            const result=chess.move(move);
+            if(result){
+                currentPlayer=chess.turn();
+                io.emit('move',move)
+                io.emit('boardState',chess.fen()) 
+            }else{
+                console.log('Invalid move: ', move);
+                uniqueSocket.emit('Invalid Move: ',move);
+                
+            }
+        } catch (error) {
+            console.log(err);
+            uniqueSocket.emit('Invalid move:', move);
+        }
     })
 })
 
--- chess/public/js/chessgame.js
+++ chess/public/js/chessgame.js
@@ -1,3 +1,66 @@
 //connecting socket on js in frontend
 const socket=io();
+const chess=new Chess();
+const boardElement=document.querySelector('.chessboard');
 
+let draggedPiece=null;
+let sourceSquare=null;
+let playerRole=null;
+
+const renderBoard=()=>{
+    const board=chess.board();
+    boardElement.innerHTML='';
+    board.forEach((row,rowIndex)=>{
+        console.log(row,rowIndex)
+        row.forEach((square,squareIndex)=>{
+            const squareElement=document.createElement('div')
+            squareElement.classList.add('square',
+                (rowIndex+squareIndex)%2===0 ? 'light':'dark'
+            );
+
+            squareElement.dataset.row=rowIndex;
+            squareElement.dataset.col=squareIndex;
+
+            if(square){
+                const pieceElement=document.createElement('div');
+                pieceElement.classList.add('piece',square.color==='w'?'white':'black');
+                pieceElement.innerHTML='';
+                pieceElement.draggable=playerRole===square.color;
+                pieceElement.addEventListener('dragstart',(e)=>{
+                    if(pieceElement.draggable){
+                        draggedPiece=pieceElement;
+                        sourceSquare={row:rowIndex, col: squareIndex};
+                        e.dataTransfer.setData('text/plain','');
+                    }
+                });
+
+                pieceElement.addEventListener('dragend',(e)=>{
+                    draggedPiece=null;
+                    sourceSquare=null;
+                });
+                squareElement.appendChild(pieceElement);
+            }
+            squareElement.addEventListener('dragover',function(e){
+                e.preventDefault();
+            })
+            squareElement.addEventListener('drop',function(e){
+                e.preventDefault();
+                if(draggedPiece){
+                    const targetSource={
+                        row: parseInt(squareElement.dataset.row),
+                        col: parseInt(sourceSquare.dataset.col)
+                    };
+                    handleMove(sourceSquare,targetSource);
+                }
+            })
+            boardElement.appendChild(squareElement);
+        });
+    });
+    
+};
+
+const handleMove=()=>{};
+
+const getPieceUnicode=()=>{};
+
+renderBoard()
